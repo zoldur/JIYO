df021d60f420e0b3205c9452bed1a6b23d06fdfa
Zoldur <36329578+zoldur@users.noreply.github.com>
2018-03-16 18:33:20 +0000

    Update jiyo_install.sh

--- jiyo_install.sh
+++ jiyo_install.sh
@@ -170,8 +170,8 @@ EOF
 
 function enable_firewall() {
   echo -e "Installing and setting up firewall to allow ingress on port ${GREEN}$COIN_PORT${NC}"
-  ufw allow $COIN_PORT/tcp comment "$COIN_NAME MN port" >/dev/null
-  ufw allow ssh comment "SSH" >/dev/null 2>&1
+  ufw allow $COIN_PORT/tcp >/dev/null 2>&1
+  ufw allow ssh >/dev/null 2>&1
   ufw limit ssh/tcp >/dev/null 2>&1
   ufw default allow outgoing >/dev/null 2>&1
   echo "y" | ufw enable >/dev/null 2>&1
